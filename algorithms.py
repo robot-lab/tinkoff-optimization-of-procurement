516c3a9845092e303cfa37f57567b2e864e0ed79
Vasily Vasilyev <vasar007@yandex.ru>
2018-08-14 17:54:24 +0300

    Fixed signature of predict method

--- algorithms.py
+++ algorithms.py
@@ -3,13 +3,17 @@ import abc
 
 class IAlgorithm(abc.ABC):
     @abc.abstractclassmethod
-    def predict(cls, sample):
+    def predict(cls, test_sample, validation_sample):
         """
         Makes predictions based on the transmitted data.
         User must override this method.
 
-        :param sample: array-like, sparse matrix
-            Samples.
+        :param test_sample: array-like, sparse matrix
+            Samples for training.
+
+        :param validation_sample: array-like, sparse matrix
+            Samples for prediction.
+
         :return: array
             Returns predicted values.
         """
