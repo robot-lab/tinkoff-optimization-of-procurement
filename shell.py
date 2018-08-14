33e22e9148a24bde91cbaa1ec6dedc7ba6a4d835
Vasily Vasilyev <vasar007@yandex.ru>
2018-08-14 18:52:35 +0300

    Added documentation comments.

--- shell.py
+++ shell.py
@@ -21,35 +21,62 @@ class Algorithm:
 
 class Shell:
     def __init__(self):
-        self.PROPORTION = 0.7
+        """
+        Constructor which initialize class fields.
+        """
+        self.__PROPORTION = 0.7
 
-        self.df = None
-        self.test_samples = None
-        self.validation_samples = None
-        self.train_samples_num = 0
-        self.prediction = None
-        self.parser = Parser()
-        self.algorithm = Algorithm()
-        self.tester = Tester()
+        self.__df = None
+        self.__test_samples = None
+        self.__validation_samples = None
+        self.__train_samples_num = 0
+        self.__prediction = None
+        self.__parser = Parser()
+        self.__algorithm = Algorithm()
+        self.__tester = Tester()
 
     def __input(self, filepath_or_buffer):
-        self.df = self.parser.parse(filepath_or_buffer, to_list=True)
-        self.train_samples_num = self.PROPORTION * len(self.df)
-        self.test_samples = self.df[:self.train_samples_num]
-        self.validation_samples = self.df[self.train_samples_num:]
+        """
+        An additional method that loads data and divides it into test and
+        validation samples.
+
+        :param filepath_or_buffer: same as Parser.parse or self.predict
+        """
+        self.__df = self.__parser.parse(filepath_or_buffer, to_list=True)
+        self.__train_samples_num = self.__PROPORTION * len(self.__df)
+        self.__test_samples = self.__df[:self.__train_samples_num]
+        self.__validation_samples = self.__df[self.__train_samples_num:]
 
     def output(self, filename="result"):
-        out = pd.DataFrame(self.prediction)
+        """
+
+        :param filename:
+        """
+        out = pd.DataFrame(self.__prediction)
         out.to_csv(f"{filename}.csv", index=False, header=False)
 
     def predict(self, filepath_or_buffer):
+        """
+        Make prediction for input dataset.
+
+        :param filepath_or_buffer: str, pathlib.Path, py._path.local.LocalPath
+            or any object with a read() method (such as a file handle or
+            StringIO)
+            The string could be a URL. Valid URL schemes include http, ftp, s3,
+            and file. For file URLs, a host is expected. For instance, a local
+            file could be file://localhost/path/to/table.csv.
+        """
         self.__input(filepath_or_buffer)
-        self.prediction = self.algorithm.predict(self.test_samples,
-                                                 self.validation_samples)
+        self.__prediction = self.__algorithm.predict(self.__test_samples,
+                                                     self.__validation_samples)
 
     def test(self):
-        self.tester.test(self.validation_samples, self.prediction)
-        self.tester.check_quality(self.validation_samples, self.prediction)
+        """
+        Test prediction quality of algorithm.
+        """
+        self.__tester.test(self.__validation_samples, self.__prediction)
+        self.__tester.quality_control(self.__validation_samples,
+                                      self.__prediction)
 
 
 def main():
