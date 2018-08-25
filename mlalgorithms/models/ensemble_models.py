1d77cbbe316438cf59a4d50dd0543a4d72216677
vasar007 <vasar007@yandex.ru>
2018-08-25 15:50:50 +0300

    Added GBM with MultiOutputRegressor.

--- mlalgorithms/models/ensemble_models.py
+++ mlalgorithms/models/ensemble_models.py
@@ -2,6 +2,9 @@ import numpy as np
 
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.ensemble import ExtraTreesRegressor
+from sklearn.ensemble import GradientBoostingRegressor
+
+from sklearn.multioutput import MultiOutputRegressor
 
 from . import model
 
@@ -26,6 +29,22 @@ class ExtraTreesModel(model.IModel):
 
     def __init__(self, **kwargs):
         self.model = ExtraTreesRegressor(**kwargs)
+
+    def train(self, train_samples, train_labels, **kwargs):
+        self.model.fit(train_samples, train_labels, **kwargs)
+
+    def predict(self, samples, **kwargs):
+        predictions = []
+        for sample in samples:
+            prediction = self.model.predict(np.array(sample).reshape(1, -1))[0]
+            predictions.append(prediction)
+        return predictions
+
+
+class GradientBoostingModel(model.IModel):
+
+    def __init__(self, **kwargs):
+        self.model = MultiOutputRegressor(GradientBoostingRegressor(**kwargs))
 
     def train(self, train_samples, train_labels, **kwargs):
         self.model.fit(train_samples, train_labels, **kwargs)
